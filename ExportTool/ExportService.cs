6da470cbca56efc725227fc65b4060fe09f018cc
Ковальчук Диана <DinDomosed@yandex.ru>
2025-07-29 00:22:10 +0300

    feat: реализован класс ExportService<T> для экспорта данных в CSV-файл
    Добавлено:
    - Библиотека классов ExportTool
    - Метод ExportToCsvFile для универсального экспорта коллекций в CSV
    - Использование CsvHelper

---
+++ ExportTool/ExportService.cs
@@ -0,0 +1,57 @@
+﻿using BankSystem.Domain;
+using BankSystem.Domain.Models;
+using System.Text;
+using CsvHelper;
+using System.Globalization;
+
+namespace ExportTool
+{
+    public class ExportService<T>
+    {
+        protected string PathToDirectory { get; private set; }
+        protected string FileName { get; private set; }
+
+        public ExportService(string pathToDirectory, string fileName)
+        {
+            PathToDirectory = pathToDirectory;
+            FileName = fileName;
+        }
+        public void ExportToCvsFile(List<T> data)
+        {
+            if (data.Count == 0 || data == null)
+                return;
+
+            DirectoryInfo dirInfo = new DirectoryInfo(PathToDirectory);
+
+            if (!dirInfo.Exists)
+                dirInfo.Create();
+
+            string fullpath = Path.Combine(PathToDirectory, FileName);
+
+            bool FileNeedsHeaders = !File.Exists(fullpath) || new FileInfo(fullpath).Length == 0;
+
+            using (FileStream fileStream = new FileStream(fullpath, FileMode.OpenOrCreate))
+            {
+                using(StreamWriter streamWriter = new StreamWriter(fileStream, new UTF8Encoding()))
+                {
+                    using (CsvWriter writer = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                    {
+                        if(FileNeedsHeaders)
+                        {
+                            writer.WriteHeader<Client>();
+                            writer.NextRecord();
+                        }
+
+                        foreach(var client in data)
+                        {
+                            writer.WriteRecord(client);
+                            writer.NextRecord();
+                        }
+
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+    }
+}
